d1a010582cfb87b7c441c3710984d4cecfa395b3
Perwez Akhtar <perwezakhtar29@gmail.com>
2024-02-14 16:28:31 +0530

    configure to postgres database

--- tour/settings.py
+++ tour/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'tour.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'tour',
-#         'USER': 'parry',
-#         'PASSWORD': 'parry',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '*1*3-2*CA*65*CdB3C6442C1eGFBdAcF',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '17155',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
